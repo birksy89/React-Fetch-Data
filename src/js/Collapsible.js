904b76ace885dd5768493766f6a2dcf70347e772
Andrew <andrew@purplecs.com>
2019-05-03 14:39:58 +0100

    Altering Colapsible Component

--- src/js/Collapsible.js
+++ src/js/Collapsible.js
@@ -1,45 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Collapsible extends React.Component {
+export default class Collapsible extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.string.isRequired,
+  };
 
-    constructor(props){
-        super(props)
-        this.state = {
-            isExpanded: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.myPanelRef = React.createRef();
+  }
 
-    handleToggle(e){
-        e.preventDefault();
-        this.setState({
-            isExpanded: !this.state.isExpanded,
-            height: this.refs.inner.clientHeight
-        })
-    }
+  state = {
+    isExpanded: false,
+  };
 
-    render(){
-        const {title, children} = this.props;
-        const {isExpanded, height} = this.state;
-        const currentHeight = isExpanded ? height : 0;
-        return (
-            <div className={`panel ${isExpanded ? 'is-expanded' : ''}`} onClick={(e) => this.handleToggle(e)}>
-                <div className="panel-heading">
-                    <h2>{title}</h2>
-                </div>
-                <div className="panel-collapse" style={{height: currentHeight+'px'}}>
-                    <div className="panel-body" ref="inner">
-                        {children}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  handleToggle = e => {
+    e.preventDefault();
 
+    console.log(this.myPanelRef.current.clientHeight);
+
+    const { isExpanded } = this.state;
+    this.setState({
+      isExpanded: !isExpanded,
+      height: this.myPanelRef.current.clientHeight,
+    });
+  };
+
+  render() {
+    const { title, children } = this.props;
+    const { isExpanded, height } = this.state;
+    const currentHeight = isExpanded ? height : 0;
+    return (
+      <div
+        className={`panel ${isExpanded ? 'is-expanded' : ''}`}
+        onClick={e => this.handleToggle(e)}
+      >
+        <div className="panel-heading">
+          <h2>{title}</h2>
+        </div>
+        <div className="panel-collapse" style={{ height: currentHeight }}>
+          <div className="panel-body" ref={this.myPanelRef}>
+            {children}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-Collapsible.propTypes = {
-    title: PropTypes.string,
-};
-
-export default Collapsible;
